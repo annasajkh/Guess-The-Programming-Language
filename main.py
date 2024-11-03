af50802c7ab41ef09dd80d43bb20033bbe4427c3
Annas <annaskherunas06@gmail.com>
2024-11-03 20:47:01 +0700

    fix game

--- main.py
+++ main.py
@@ -1,128 +1,123 @@
 import requests
 import random
-import re
 import os
 import time
+import click
+
+def clear():
+    click.clear()
 
 def get_repos():
-  return requests.get(f"https://api.github.com/gists/public?page={random.randrange(0,100)}").json()
+    return requests.get(f"https://api.github.com/gists/public?page={random.randrange(0, 100)}").json()
 
 
 code_len = 35
 
 
 def get_rand_repo(repos):
-	if not repos:
-		return None
+    if not repos:
+        return None
 
-	repo = repos[random.randrange(len(repos))]
+    repo = repos[random.randrange(len(repos))]
 
+    while "files" not in repo.keys():
+        repos.remove(repo)
 
-	while "files" not in repo.keys():
-		repos.remove(repo)
+        if not repos:
+            return None
 
-		if not repos:
-			return None
+        repo = repos[random.randrange(len(repos))]
 
-		repo = repos[random.randrange(len(repos))]
-
-
-	return repo
-
+    return repo
 
 
 def get_rand_file(repo):
-	if not repo:
-		return None, None, None
-	
-	files = repo["files"]
+    if not repo:
+        return None, None, None
 
-	if not files:
-			return None, None, None
+    files = repo["files"]
 
-	file_keys = list(files.keys())
+    if not files:
+        return None, None, None
 
-	key = random.choice(file_keys)
-	file = files[key]
+    file_keys = list(files.keys())
 
-	content = requests.get(file["raw_url"]).text.strip()
+    key = random.choice(file_keys)
+    file = files[key]
 
-	while file["language"] == None or file["language"] == "Text" or len(content) < 100 or "\n" not in content:
-		files.pop(key)
-		file_keys.remove(key)
+    content = requests.get(file["raw_url"]).text.strip()
 
-		if not files:
-			return None, None, None
+    while file["language"] == None or file["language"] == "Text" or len(content) < 100 or "\n" not in content:
+        files.pop(key)
+        file_keys.remove(key)
 
-		key = random.choice(file_keys)
-		file = files[key]
-		content = requests.get(file["raw_url"]).text
+        if not files:
+            return None, None, None
 
+        key = random.choice(file_keys)
+        file = files[key]
+        content = requests.get(file["raw_url"]).text
 
-	return key, file, content
-
+    return key, file, content
 
 
 while True:
-	repos = get_repos()
+    repos = get_repos()
 
-	try:
-		for i in range(0,5):
-			repo = get_rand_repo(repos)
+    try:
+        for i in range(0, 5):
+            repo = get_rand_repo(repos)
 
-			while not repo:
-				repo = get_rand_repo(repos)
+            while not repo:
+                repo = get_rand_repo(repos)
 
-			key, file, content = get_rand_file(repo)
+            key, file, content = get_rand_file(repo)
 
-			while not file:
-				repos.remove(repo)
-				repo = get_rand_repo(repos)
-				key, file, content = get_rand_file(repo)
+            while not file:
+                repos.remove(repo)
+                repo = get_rand_repo(repos)
+                key, file, content = get_rand_file(repo)
 
-			if not repos:
-				break
+            if not repos:
+                break
 
+            codes = content.split("\n")
 
-			codes = content.split("\n")
+            rand_section = random.randrange(len(codes) - code_len) if len(codes) > code_len else 0
 
-			rand_section = random.randrange(len(codes) - code_len) if len(codes) > code_len else 0
+            while True:
+                print("_" * 150)
+                chunks = codes[rand_section:rand_section + code_len]
 
-			while True:
-				print("_" * 150)
-				chunks = codes[rand_section :rand_section + code_len]
+                print("\n".join(chunks))
+                print("_" * 150)
 
-				print("\n".join(chunks))
-				print("_" * 150)
+                answer = input("what language is this? ").lower().strip()
 
-				answer = input("what language is this? ").lower().strip()
-				
-				if answer == file["language"].lower():
-					break
+                if answer == file["language"].lower():
+                    break
 
-				if answer == "idk":
-					print("the answer is " + file["language"])
-					break
-				elif answer == "quit":
-					os.system("clear")
-					exit(0)
+                if answer == "idk":
+                    print("the answer is " + file["language"])
+                    break
+                elif answer == "quit":
+                    clear()
+                    exit(0)
 
-				print("WRONG try again")
+                print("WRONG try again")
 
-				time.sleep(1)
-				os.system("clear")
+                time.sleep(1)
+                clear()
 
+            if answer != "idk":
+                print("you are right congrats")
 
-			if answer != "idk":
-				print("you are right congrats")
+            time.sleep(2)
 
-			time.sleep(2)
-			
-			os.system("clear")
+            clear()
 
-
-			if not repos:
-				print("there is no files left.....")
-				break
-	except:
-		pass
+            if not repos:
+                print("there is no files left.....")
+                break
+    except Exception as e:
+        print(e)
